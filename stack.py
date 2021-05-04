66c3527796e01be08b2c5a098e16cefb7f8f8617
Oleksandr Sirenko <oleksa.sirenko@gmail.com>
2021-05-04 20:29:16 +0300

    Add stack module. Implement the basic data structure logic.

---
+++ stack.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data, next_node):
+        self.data = data
+        self.next_node = next_node
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.size = 0
+
+    def get_size(self):
+        return self.size
+
+    def peek(self):
+        return self.top
+
+    def push(self, data):
+        next_node = self.top
+        new_top = Node(data, next_node)
+        self.top = new_top
+        self.size += 1
+
+    def pop(self):
+        if self.top is None:
+            return None
+        removed = self.top
+        self.top = self.top.next_node
+        self.size -= 1
+        return removed
